51ec99d039494de5a918525920ea23471f271279
Adrian <adrianjewell91@gmail.com>
2017-08-21 15:38:35 -0700

    first project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,56 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save!
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    if @user
+      render json: @user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+
+    @user = User.find(params[:id])
+    # byebug
+    if @user.update_attributes(user_params)
+      render json: @user
+    else
+      render @user.errors.full_message, status: 422
+    end
+
+  end
+
+  def destroy
+
+    @user = User.find(params[:id])
+    if @user.destroy
+      render plain: "Destroyed"
+    else
+      render plain: "Can't find user!"
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create(name: Faker::Name.name, email: Faker::Internet.email)
+User.create(name: Faker::Name.name, email: Faker::Internet.email)
+User.create(name: Faker::Name.name, email: Faker::Internet.email)
+User.create(name: Faker::Name.name, email: Faker::Internet.email)
+User.create(name: Faker::Name.name, email: Faker::Internet.email)
